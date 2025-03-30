b778b67f8571c4cb6cc62421a7cf8b9c69769d0e
DominicBarnett <dominic.barnett@icloud.com>
2025-03-30 10:45:11 -0700

    basics done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { useState } from 'react';
 import Counter from './Counter';
 
 function App() {
+  const [counts, setCounts] = useState([1, 4, 3]);
+
+  const updateCount = (index, delta) => {
+    const newCounts = [...counts]; // Copy array (immutability)
+    newCounts[index] += delta;
+    setCounts(newCounts);
+  };
+
   return (
     <div className="App">
-      <Counter label="Apples" value={1} />
-      <Counter label="Oranges" value={3} />
+      {counts.map((value, index) => (
+        <Counter
+          key={index}
+          label={`Counter ${index + 1}`}
+          value={value}
+          increment={() => updateCount(index, 1)}
+          decrement={() => updateCount(index, -1)}
+        />
+      ))}
     </div>
   );
 }
